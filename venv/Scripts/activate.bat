4d146d197a10cae0b4561ff866d1d428f91b6337
Aravind Itte <aravinditte0121@gmail.com>
2025-02-02 22:20:04 +0530

    venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\New folder (2)\AIpytutor\venv
+set VIRTUAL_ENV=D:\New folder (3)\AIpytutor\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
